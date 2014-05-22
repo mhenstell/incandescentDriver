c13c640f4635bfb94376c0f6f4e5ab47c0c0b4a6
Max Henstell <max@kapamaki.net>
2014-05-21 22:56:16 -0400

    test firmware/host

--- arduinoFirmware/arduinoFirmware.ino
+++ arduinoFirmware/arduinoFirmware.ino
@@ -1,25 +1,25 @@
-#include <ShiftPWM.h>   // include ShiftPWM.h after setting the pins!
 
-#define LED_CONFIG	(DDRD |= (1<<6))
-#define LED_ON		(PORTD |= (1<<6))
-#define LED_OFF		(PORTD &= ~(1<<6))
-#define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
+//#define LED_CONFIG	(DDRD |= (1<<6))
+//#define LED_ON		(PORTD |= (1<<6))
+//#define LED_OFF		(PORTD &= ~(1<<6))
+//#define CPU_PRESCALE(n) (CLKPR = 0x80, CLKPR = (n))
 #define CDC_RX_ENDPOINT		3
-
+//
 int packetNum = 0;
-int numRegisters = 37;
+
+
+const int ShiftPWM_latchPin=8;
+
+const bool ShiftPWM_invertOutputs = false; 
+
+const bool ShiftPWM_balanceLoad = false;
+#include <ShiftPWM.h>   // include ShiftPWM.h after setting the pins!
 
 unsigned char maxBrightness = 255;
 unsigned char pwmFrequency = 75;
+int numRegisters = 6;
 
-const int ShiftPWM_latchPin=8;
-// If your LED's turn on if the pin is low, set this to true, otherwise set it to false.
-const bool ShiftPWM_invertOutputs = false;
-
-// You can enable the option below to shift the PWM phase of each shift register by 8 compared to the previous.
-// This will slightly increase the interrupt load, but will prevent all PWM signals from becoming high at the same time.
-// This will be a bit easier on your power supply, because the current peaks are distributed.
-const bool ShiftPWM_balanceLoad = false;
+float x = 0;
 
 void setup()
 {
@@ -35,25 +35,26 @@ void setup()
 void loop()
 {
 
-  LED_CONFIG;
-  CPU_PRESCALE(0);
+//  LED_CONFIG;
+//  CPU_PRESCALE(0);
+
 
   while (1)
   {
     const int8_t n = Serial.available();
     if (n <= 0) {
-      LED_OFF;
+//      LED_OFF;
       continue;
     }
 
-    if (n == 51) packetNum = 0;
-    else if (n == 49) packetNum = 1;
-    else if (n == 48) packetNum = 2;
-
-    int offset = 0;
-    if (packetNum == 0) offset = 0;
-    else if (packetNum == 1) offset = 51;
-    else if (packetNum == 2) offset = 100;
+//    if (n == 51) packetNum = 0;
+//    else if (n == 49) packetNum = 1;
+//    else if (n == 48) packetNum = 2;
+//
+//    int offset = 0;
+//    if (packetNum == 0) offset = 0;
+//    else if (packetNum == 1) offset = 51;
+//    else if (packetNum == 2) offset = 100;
 
     const uint8_t irq_flags = SREG;
     cli();
@@ -63,7 +64,7 @@ void loop()
     {
       UENUM = CDC_RX_ENDPOINT;
       const uint8_t c = UEDATX;
-      ShiftPWM.SetOne(offset + i, c);
+      ShiftPWM.SetOne(i, c);
     }
 
     // Release the USB buffer
--- python_host.py
+++ python_host.py
@@ -1,6 +1,8 @@
 import socket
 import time
 import serial
+import math
+from numpy import interp
 
 
 # UDP settings
@@ -15,21 +17,41 @@ sock.bind((host,port))
 
 with serial.Serial(ringInterface, 115200, timeout=0) as ser:
 
+	# while True:
+	# 	data, addr = sock.recvfrom(buffer_size)
+	# 	if ord(data[0]) != 1:
+	# 		print "RX Error"
+	# 		continue
+
+	# 	ringData = data[1:-1]
+	# 	bulbData = data[-1]
+
+	# 	packet1 = ringData[0:51]	# len 51
+	# 	packet2 = ringData[51:100]	# len 49
+	# 	packet3 = ringData[100:148]	# len 48
+
+	# 	ser.write(packet1)
+	# 	ser.write(packet2)
+	# 	ser.write(packet3)
+
+	# 	print "sending"
+
+	q = 0
+
 	while True:
-		data, addr = sock.recvfrom(buffer_size)
-		if ord(data[0]) != 1:
-			print "RX Error"
-			continue
 
-		ringData = data[1:-1]
-		bulbData = data[-1]
+		ringData = [0 for x in range(0, 148)]
+		
+		for x in range(0, 148):
+			sin = math.sin(x + q)
+			ringData[x] = int(interp(sin, [-1, 1], [0, 255]))
 
-		packet1 = ringData[0:51]	# len 51
-		packet2 = ringData[51:100]	# len 49
-		packet3 = ringData[100:148]	# len 48
+		data = ringData[0:4]
 
-		ser.write(packet1)
-		ser.write(packet2)
-		ser.write(packet3)
+		ser.write(data)
 
+		q += 0.1
+
+
+		
 
